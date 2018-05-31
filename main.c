60b45ad86bf6253213b87a406c4efd8b9f2a5946
川 <gaochuan@153.buffer.buffer.oss.bjs.p1staff.com>
2018-05-31 22:00:36 +0800

    add INT signal suppurt

--- main.c
+++ main.c
@@ -34,6 +34,17 @@ struct timer_data
    struct connection ** conns;
    unsigned int  conns_num;
 };
+
+struct param
+{
+   char          *ip;
+   char          *path;
+   char          *postdata;
+   char          *method;
+   unsigned int  port;
+   unsigned int  n;
+};
+
 struct connection 
 {
     unsigned int id;// connection id
@@ -47,22 +58,18 @@ struct connection
     char         host[MAX_HOST_LEN];
     unsigned int port;
     unsigned int fd;
+    struct param * param;
 };
 
 
-struct Host
-{
-   char *ip;
-   unsigned int  port;
-   unsigned int  n;
-   char *path;
-};
 
-struct   Host h;
+
+struct timer_data timer_data;
+struct   param param;
 unsigned int  g_connection_id = 0;
 
-struct connection *  new_tcp_connection_chain(char * ip, unsigned int port,struct ev_loop *main_loop);
-int continue_tcp_connection(struct connection *conn, struct ev_loop *main_loop);
+struct connection *  new_connection_chain(struct param * param,struct ev_loop *main_loop);
+int continue_connection(struct connection *conn, struct ev_loop *main_loop);
 
 long getCurrentTime()    
 {    
@@ -87,7 +94,7 @@ int setnonblock(int sockfd)
     DEBUG_PRINT("set non block ok\n");
     return 0;
 }
-static int new_tcp_connection(const char *ip, unsigned int port)
+static int new_connection(const char *ip, unsigned int port)
 {
 
     int ret;
@@ -111,72 +118,66 @@ static int new_tcp_connection(const char *ip, unsigned int port)
     return fd;
 }
 
-char * build_http_request(const char* method, char *url,const char *body)
+int   build_http_request(struct connection * conn)
 {
-       char * buf = malloc(WRITE_BUF_LEN);
-
-       if (buf == NULL)
-       {
-           return NULL; 
-       }
+       char * method = conn->param->method;
+       char * path   = conn->param->path;
+       char * body   = conn->param->postdata;
+       char * ip     = conn->param->ip;
+       char * buf    = conn->writebuf;
 
        strcpy(buf,method);
        strcat(buf," ");
-       strcat(buf,h.path);
+       strcat(buf,path);
        strcat(buf," HTTP/1.1\r\n");
        strcat(buf,"User-Agent: hyc\r\n");
        strcat(buf,"Host: ");
-       strcat(buf, h.ip);
+       strcat(buf,ip);
        strcat(buf,"\r\n");
        strcat(buf,"Accept: */*\r\n");
 
        if (strcmp("GET",method) == 0)
        {
-        strcat(buf,"\r\n");
+          strcat(buf,"\r\n");
        }
        else if(strcmp("POST",method) ==0)
        {
-	     strcat(buf,"Content-Length: ");
-	     char contentlen[10] = {0};
-	     sprintf(contentlen,"%d",strlen(body));
-	     strcat(buf,contentlen);
+	      strcat(buf,"Content-Length: ");
+	      char contentlen[10] = {0};
+	      sprintf(contentlen,"%d",strlen(body));
+	      strcat(buf,contentlen);
 
-	     strcat(buf,"\r\n\r\n");
-	     strcat(buf,body);
+	      strcat(buf,"\r\n\r\n");
+	      strcat(buf,body);
        }
        else
        {
 	       DEBUG_PRINT("Current not supoorted method","");
-	       free(buf);
-	       return NULL;
+	       return -1;
        }
        /* 
        DEBUG_PRINT("request built:\n----------------------------"\
                    "------------------------------\n%s\n---------"\
                    "-------------------------------------------------\n",buf);
        */ 
-       return buf;
+       return 0;
 }
-  
-void http_read(struct ev_loop *loop, ev_io *stat, int events)
+int flush_connection(struct connection * conn)
+{   
+    int writelen = write(conn->fd,conn->writebuf,strlen(conn->writebuf));
+    DEBUG_PRINT("send:\n%s\n",conn->writebuf);
+    conn->request_send_timestamp = getCurrentTime();
+
+    if (errno)
+    {
+        PRINT("fd:%d writelen:%d errno:%d desc:%s\n",conn->fd, writelen,errno,strerror(errno));
+        exit(-1);
+    }
+    return 0;
+} 
+int receive_connection(struct connection *conn)
 {
-   struct connection *conn =  stat->data;
-
-   int n = read(stat->fd,conn->readbuf, READ_BUF_LEN);
-   if (n == 0)
-   {
-        PRINT("remote closed client\n") ;
-        close(stat->fd);
-        ev_io_stop(loop, stat);
-        free(stat);
-
-        PRINT("reconnect to  client:%s:%d\n",h.ip,h.port) ;
-        continue_tcp_connection(conn ,loop);
-   }
-   else
-   {
-	   conn->request_count ++; 
-
+        conn->request_count ++; 
        
 	   DEBUG_PRINT("conn[%d] http request cnt: %d\n",conn->id, conn->request_count);
 	   DEBUG_PRINT("recv:\n"\
@@ -186,24 +187,41 @@ void http_read(struct ev_loop *loop, ev_io *stat, int events)
        
         conn->request_total_time += (getCurrentTime()- conn->request_send_timestamp );
 
-       if(conn->request_count >= h.n )
-	   {
-            PRINT("conn:%d run %d times quit,set :%d\n",conn->id, conn->request_count,h.n);
+}
+void http_read(struct ev_loop *loop, ev_io *stat, int events)
+{
+   struct connection *conn =  stat->data;
 
+   int n = read(stat->fd,conn->readbuf, READ_BUF_LEN);
+   if (n >0)
+   {
+       receive_connection(conn);
+       if(conn->param->n > 0 && conn->request_count >= conn->param->n )
+	   {
+            PRINT("conn:%d run %d times quit,set :%d\n",conn->id, conn->request_count,conn->param->n);
             close(stat->fd);
             ev_io_stop(loop, stat);
             free(stat);
             return; 
 	   }
 
-       DEBUG_PRINT("send:\n%s\n",conn->writebuf);
-	   write(stat->fd,conn->writebuf,strlen(conn->writebuf));
-       conn->request_send_timestamp = getCurrentTime();
+       flush_connection(conn);
    }
+   else
+   {
+        PRINT("remote closed client\n") ;
+        close(stat->fd);
+        ev_io_stop(loop, stat);
+        free(stat);
+
+        PRINT("reconnect to  client:%s:%d\n",conn->param->ip,conn->param->port) ;
+        continue_connection(conn ,loop);
+   }
+
 }
-int continue_tcp_connection(struct connection *conn, struct ev_loop *main_loop)
+int continue_connection(struct connection *conn, struct ev_loop *main_loop)
 {
-   int  fd = new_tcp_connection(conn->host,conn->port);
+   int  fd = new_connection(conn->host,conn->port);
 
     if (fd < 0 )
     {
@@ -226,21 +244,16 @@ int continue_tcp_connection(struct connection *conn, struct ev_loop *main_loop)
     conn->id   = g_connection_id++ ; 
     conn->fd   = fd;
 
-    int ret = write(fd,conn->writebuf,strlen(conn->writebuf));
-    conn->request_send_timestamp = getCurrentTime();
-    if( errno == EAGAIN)// 实际测试中第一次也会出现反回EAGIN的情况，这时buf是第一次写入数据，why?
-    {
-        DEBUG_PRINT(" ret %d errno:%d desc:%s\n",ret,errno,strerror(errno));
-    }
-    else if(errno != EAGAIN)
-    {
-        DEBUG_PRINT(" ret %d errno:%d desc:%s\n",ret,errno,strerror(errno));
-    }
+    flush_connection(conn);
 
 }
-struct connection * new_tcp_connection_chain(char * ip, unsigned int port,struct ev_loop *main_loop)
+
+struct connection * new_connection_chain(struct param * param,struct ev_loop *main_loop)
 {
-    int  fd = new_tcp_connection(ip,port);
+    char * ip     = param->ip;
+    unsigned port = param->port;
+
+    int  fd = new_connection(ip,port);
 
     if (fd < 0 )
     {
@@ -269,26 +282,14 @@ struct connection * new_tcp_connection_chain(char * ip, unsigned int port,struct
     conn->request_count        = 0;
     conn->request_total_time   = 0;
     conn->id                   = g_connection_id++ ; 
+    conn->param                = param;
 
     ev_io_init(http_readable,http_read,fd,EV_READ);
     http_readable->data = conn;
     ev_io_start(main_loop,http_readable);
 
-    char *req = build_http_request("GET",h.path,"");
-    strncpy(conn->writebuf,req,WRITE_BUF_LEN);
-    free(req);
-
-
-    int ret = write(fd,conn->writebuf,strlen(conn->writebuf));
-    conn->request_send_timestamp = getCurrentTime();
-    if( errno == EAGAIN)// 实际测试中第一次也会出现反回EAGIN的情况，这时buf是第一次写入数据，why?
-    {
-        PRINT("fd:%d ret %d errno:%d desc:%s\n",fd, ret,errno,strerror(errno));
-    }
-    else if(errno != EAGAIN)
-    {
-        PRINT("fd:%d ret %d errno:%d desc:%s\n",fd, ret,errno,strerror(errno));
-    }
+    build_http_request(conn);
+    flush_connection(conn);
     return conn;
 }
 static void timer_callback(struct ev_loop *loop,ev_timer *w,int revents)
@@ -322,21 +323,54 @@ static void timer_callback(struct ev_loop *loop,ev_timer *w,int revents)
     printf("Request Latency  : %f ms\n",(float)request_total_time/request_cnt);
     exit(0);
 }
+void stop(int sig)
+{   
+    int request_cnt          = 0;
+    int request_total_time   = 0;
+    int request_qps          = 0;
+    struct timer_data * data = &timer_data;
+ 
+    for(int i=0; i< data->conns_num; i++)
+    {
+        int request_cnt_tmp        = data->conns[i]->request_count; 
+        int request_total_time_tmp = data->conns[i]->request_total_time; 
+        int request_qps_tmp        = (int)((float)(request_cnt_tmp)/((float)(request_total_time_tmp)/1000));
 
+        printf("Connection %3d Result:\n",data->conns[i]->id);
+        printf("Total SendRequest: %d\n",request_cnt_tmp);
+        printf("Total Time(ms)   : %d\n",request_total_time_tmp);
+        printf("Total QPS        : %d\n",request_qps_tmp);
+        printf("Request Latency  : %f ms\n",(float)request_total_time_tmp/request_cnt_tmp);
+        request_qps                += request_qps_tmp;
+        request_cnt                += data->conns[i]->request_count; 
+        request_total_time         += data->conns[i]->request_total_time; 
+    }
+    
+    printf("\nTotal Result:\n");
+    printf("Total SendRequest: %d\n",request_cnt);
+    printf("Total Time(ms)   : %d\n",request_total_time);
+    printf("Total QPS        : %d\n",request_qps);
+    printf("Request Latency  : %f ms\n",(float)request_total_time/request_cnt);
+    exit(0);
+
+
+}
 int main(int argc , char ** argv)
 {
 
     int i = 0;
     int c ;
-    int n = 10000;
+    int n = -1;
     unsigned int  t = 10;
     unsigned int  concurrent  = 0;
     unsigned int  port  = 0;
     char * url  = NULL;
     char * host = NULL;
+    char * method = NULL;
+    char * postdata = NULL;
 
     opterr = 0;
-    while( (c= getopt(argc, argv , "c:u:n:h:p:t:")) != -1 )
+    while( (c= getopt(argc, argv , "c:u:n:h:p:t:X:d:")) != -1 )
     {
    	switch(c)
 	    {
@@ -351,6 +385,12 @@ int main(int argc , char ** argv)
 			break;
 	   	case 'n': 
 			n = atoi(optarg);
+			break;
+	   	case 'X': 
+			method = optarg;
+			break;
+	   	case 'd': 
+			postdata = optarg;
 			break;
 	   	case 't': 
 			t = atoi(optarg);
@@ -372,24 +412,26 @@ int main(int argc , char ** argv)
        exit(-1); 
     }
 
+    signal(SIGINT, stop);
 
-    h.ip = host;
-    h.port = port;
-    h.path = url;
-    h.n = n;
+    param.ip = host;
+    param.port = port;
+    param.path = url;
+    param.n = n;
+    param.postdata= postdata;
+    param.method = method?method:"GET";
 
-    DEBUG_PRINT("%s:%d concurrent:%d\n",h.ip,h.port,concurrent);
+    DEBUG_PRINT("%s:%d concurrent:%d\n",param.ip,param.port,concurrent);
 
     struct ev_loop *main_loop = ev_default_loop(0);
     
-    struct timer_data timer_data;
 
     timer_data.conns = malloc(sizeof(struct connection *)*concurrent);
     timer_data.conns_num = concurrent;
 
     for(i = 0; i< concurrent; i++)
     {
-	   timer_data.conns[i] = new_tcp_connection_chain(h.ip, h.port ,main_loop);
+	   timer_data.conns[i] = new_connection_chain(&param ,main_loop);
     }
 
     if ( t > 0 )
@@ -401,4 +443,5 @@ int main(int argc , char ** argv)
         ev_timer_start(main_loop,&timer_watcher);
     }
     ev_run(main_loop, 0);
+    free(timer_data.conns);
 }
